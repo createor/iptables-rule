9722ddfb57de3409f523e5e5e0910bf91236ee13
createor <40543429+createor@users.noreply.github.com>
2024-05-17 18:34:35 +0800

    Update startup.sh
    add port config

--- startup.sh
+++ startup.sh
@@ -2,8 +2,9 @@
 # @Desc: 启动脚本
 # @Time: 2024/05/17
 
+DEFAULT_PORT=8089  # 服务监听端口
 BASE_DIR=$(cd $(dirname $0);pwd)
 PID_FILE=${BASE_DIR}/rule.pid
-nohup rule > /dev/null 2>&1 &
+nohup rule -p ${DEFAULT_PORT} > /dev/null 2>&1 &
 echo "$!" > ${PID_FILE}
 exit 0
