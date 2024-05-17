8f9a0b4809291c1b5698ec4aeffaf7f91e193be0
createor <40543429+createor@users.noreply.github.com>
2024-05-17 18:42:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# @Desc：编译并制作deb\rpm安装包
+# @Desc：编译成二进制并制作deb、rpm安装包
 # @Time: 2024/05/13
 
 VERSION=0.0.1  # 版本
@@ -14,7 +14,7 @@ fi
 export CGO_ENABLED=0
 export GOOS=linux
 
-# uname -i 获取cpu架构
+# uname -i 获取系统架构
 case "${1}" in
     "i386")  # 32位
       export AECH=i386
@@ -39,10 +39,7 @@ case "${2}" in
       sed -i "s/Architecture:[[:space:]]\S*/Architecture: ${1:-${DEFAULT_ARCH}}/" dist/rule/DEBIAN/control  # 替换架构
       # 拷贝文件
       cp -f rule dist/rule/usr/local/bin/rule
-      cp -f startup.sh dist/rule/usr/local/share/rule
-      cp -f shutdown.sh dist/rule/usr/local/share/rule
-      cp -f rule.service dist/rule/usr/local/share/rule
-      cp -rf html dist/rule/usr/local/share/rule/
+      cp -rf html startup.sh shutdown.sh rule.service dist/rule/usr/local/share/rule
       # 修改权限
       chmod -R 0755 dist/rule/
       sudo dpkg -b dist/rule rule_${VERSION}-${1:-${DEFAULT_ARCH}}.deb
@@ -52,7 +49,7 @@ case "${2}" in
       # 生成rpm安装包
       # 请在Centos系统上执行
       # go build -ldflags="-buildid" -o rule
-      go build -ldflags="-s -w" -o rule
+      go build -ldflags="-s -w" -o rule  # 编译
       sed -i "s/Version[[:space:]]\S*/Version: ${VERSION}/" dist/rpmbuild/SPECS/rule.spec  # 替换版本字段
       mkdir rule-${VERSION}
       cp -rf rule html startup.sh shutdown.sh rule.service rule-${VERSION}/  # 拷贝文件
@@ -61,7 +58,7 @@ case "${2}" in
       mv rule-${VERSION}.tar.gz dist/rpmbuild/SOURCES/  # 移动文件
       # rpmbuild只能从/root/目录下操作
       cp -rf dist/rpmbuild ~/
-      sudo rpmbuild -ba ~/rpmbuild/SPECS/rule.spec  # rpm生成在/root/rpmbuild/RPMS目录下
+      sudo rpmbuild -ba ~/rpmbuild/SPECS/rule.spec  # rpm安装包文件生成在/root/rpmbuild/RPMS目录下
     ;;
     *)
       echo "错误的'系统'参数"
